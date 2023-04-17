bf407882a51e7c7c2dff5c94b738e69cc75994fe
VSarabudla <woodsofnarnia@gmail.com>
2023-04-17 17:19:33 +0200

    Add hero sprite to game background

--- src/RegimeMain.java
+++ src/RegimeMain.java
@@ -17,6 +17,30 @@ import javafx.stage.Stage;
 public class RegimeMain extends Application {
         @Override
         public void start(Stage primaryStage) throws Exception {
+            primaryStage.setTitle("Back to 1789");
+            Group root = new Group();
+            Group gameRoot = new Group();
+            Scene scene = new Scene(root, 1600, 800,true);
+            Scene nextScene = new Scene(gameRoot, 1600,800,true);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            Pane pane = new Pane();
+            Image background = new Image("file:./resources/images/game_background-transformed.jpeg");
+            ImageView backgroundView=new ImageView(background);
+            backgroundView.setViewport(new Rectangle2D(0,0,1600,800));
+            root.getChildren().add(pane);
+            pane.getChildren().add(backgroundView);
+
+            Image FDR = new Image("file:./resources/images/hero_right_walk.png");
+            ImageView heroView = new ImageView(FDR);
+            heroView.setViewport(new Rectangle2D(2124,0,708,929));
+            pane.getChildren().add(heroView);
+            heroView.setScaleX(0.25);
+            heroView.setScaleY(0.25);
+            heroView.setX(800);
+            heroView.setY((background.getHeight())/10);
+
             AnimationTimer timer = new AnimationTimer() {
                 @Override
                 public void handle(long time) {
