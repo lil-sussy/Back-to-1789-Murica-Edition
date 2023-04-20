feeb949dc78ed65fd1b4725ea884b93b9dd2bd7e
VSarabudla <woodsofnarnia@gmail.com>
2023-04-20 17:14:17 +0200

    Add walk animation

--- src/Hero.java
+++ src/Hero.java
@@ -18,11 +18,15 @@ public class Hero {
 
     private Point2D startingSprite;
 
+    private Point2D currentFrame;
+
     private boolean isWalkingLeft = false;
     private boolean isWalkingRight = false;
+    private Point2D walkDest = new Point2D(0, 0);
+    private int frames;
 
 
-    public Hero(Image startingImage, int spriteColumnSize, int spriteRowSize, int x, int y, Point2D startingSprite) {
+    public Hero(Image startingImage, int spriteColumnSize, int spriteRowSize, int x, int y, Point2D startingSprite, int frames) {
         this.heroView = new ImageView(startingImage);
         this.spriteColumnSize = spriteColumnSize;
         this.spriteRowSize = spriteRowSize;
@@ -30,23 +34,51 @@ public class Hero {
         this.x = x;
         this.y = y;
         this.startingSprite = startingSprite;
+        this.currentFrame = startingSprite;
+        this.frames = frames;
     }
 
     public void updateImageViewInScene(long time) {
         if (isWalkingLeft) {
             x -= 1;
+            if (currentFrame.getX() <= 0) {
+                currentFrame = new Point2D(frames, 0);
+            } else {
+                currentFrame = new Point2D(currentFrame.getX() - 1, 0);
+            }
+            this.getHeroView().setViewport(new Rectangle2D(currentFrame.getX() * this.getSpriteColumnSize(), 0, this.getSpriteColumnSize(), this.getSpriteRowSize()));
+            if (x < walkDest.getX()) {
+                isWalkingLeft = false;
+                this.getHeroView().setViewport(new Rectangle2D(3 * this.getSpriteColumnSize(), 0, this.getSpriteColumnSize(), this.getSpriteRowSize()));
+            }
         } else if (isWalkingRight) {
             x += 1;
+            if (currentFrame.getX() >= frames) {
+                currentFrame = new Point2D(0, 0);
+            } else {
+                currentFrame = new Point2D(currentFrame.getX() + 1, 0);
+            }
+            this.getHeroView().setViewport(new Rectangle2D(currentFrame.getX() * this.getSpriteColumnSize(), this.getSpriteRowSize(), this.getSpriteColumnSize(), this.getSpriteRowSize()));
+            if (x > walkDest.getX()) {
+                isWalkingRight = false;
+                this.getHeroView().setViewport(new Rectangle2D(3 * this.getSpriteColumnSize(), this.getSpriteRowSize(), this.getSpriteColumnSize(), this.getSpriteRowSize()));
+            }
         }
         this.heroView.setX(x);
     }
 
     public void walkLeft(Point2D dest) {
-        isWalkingLeft = true;
+        this.getHeroView().setViewport(new Rectangle2D(3 * this.getSpriteColumnSize(), 0, this.getSpriteColumnSize(), this.getSpriteRowSize()));
+        this.isWalkingLeft = true;
+        this.isWalkingRight = false;
+        this.walkDest = dest;
     }
 
     public void walkRight(Point2D dest) {
-        isWalkingRight = true;
+        this.getHeroView().setViewport(new Rectangle2D(3 * this.getSpriteColumnSize(), this.getSpriteRowSize(), this.getSpriteColumnSize(), this.getSpriteRowSize()));
+        this.isWalkingRight = true;
+        this.isWalkingLeft = false;
+        this.walkDest = dest;
     }
 
     public ImageView getHeroView() {
